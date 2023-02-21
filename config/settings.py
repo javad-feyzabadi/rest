148bcfa5658c65baa8cb51bc101a062873ab8829
javad <javad.feyzabadi@yahoo.com>
2023-02-21 20:41:58 +0330

    create accounts app

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'accounts.apps.AcoountsConfig',
+
     'rest_framework',
 ]
 
