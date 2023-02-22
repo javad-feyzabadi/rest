93e7fa989b1ca8e31b98e2f620269dd1ee73b014
javad <javad.feyzabadi@yahoo.com>
2023-02-22 22:45:52 +0330

    status code

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
 
 from . serializers import UserRegisterSerializer
 
@@ -12,7 +14,7 @@ class UserRegister(APIView):
         ser_data = UserRegisterSerializer(data=request.POST) 
         if ser_data.is_valid():
             ser_data.create(ser_data.validated_data)
-            return Response(ser_data.data)
-        return Response(ser_data.errors)
+            return Response(ser_data.data,status= status.HTTP_201_CREATED)
+        return Response(ser_data.errors,status= status.HTTP_400_BAD_REQUEST)
 
 
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from . models import Person
 from . serializers import PersonSerializer
@@ -8,7 +9,7 @@ class Home(APIView):
     def get(self,request):
         person = Person.objects.all()
         ser_data = PersonSerializer(instance=person,many = True)
-        return Response(data=ser_data.data)
+        return Response(data=ser_data.data,status= status.HTTP_200_OK)
 
 
 
