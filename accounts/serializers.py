f3dfd76678909833e60637756e45e93a29187089
javad <javad.feyzabadi@yahoo.com>
2023-02-22 21:35:31 +0330

    create validations

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
 
 
+def clean_email(value):
+    if 'admin' in value:
+       raise serializers.ValidationError('you cant select admin')
+    return value
+
+
 class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required = True)
-    email = serializers.EmailField(required = True)
-    password = serializers.CharField(required = True,write_only = True)
+    email = serializers.EmailField(required = True,validators = [clean_email])
+    password1 = serializers.CharField(required = True,write_only = True)
+    password2 = serializers.CharField(required = True,write_only = True)
+
+
+    def validate_username(self,value):
+        if value == 'admin':
+            raise serializers.ValidationError('you cant select admin')
+        return value
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError('password dose not match')
+        return data
