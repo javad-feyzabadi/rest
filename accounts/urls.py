6108270d3c65a41c0d724c84e191a8ba04afc34c
javad <javad.feyzabadi@yahoo.com>
2023-02-22 23:26:36 +0330

    token authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from . import views
+
+from rest_framework.authtoken import views
+
+
+from .views import UserRegister
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/',views.UserRegister.as_view(),name='register'),
+    path('register/',UserRegister.as_view(),name='register'),
+    path('api-token-auth/', views.obtain_auth_token),
+
 ]
--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
 
     'rest_framework',
+
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# REST_FRAMEWORK AUTHENTICATION
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
